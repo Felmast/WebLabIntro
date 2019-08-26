6d306ecb27eaf2de83fe82716d2012fcaaa5506c
Felmast <37111047+Felmast@users.noreply.github.com>
2019-08-26 11:56:58 -0600

    Add files via upload

---
+++ data/practices/fullPracticeList.js
@@ -0,0 +1,12 @@
+﻿let practice1 = {name:"Semana 3:Sintaxis HTML5 y CSS3.",
+	date:"12/08/2019.",
+	filename:"primeraTarea.txt",
+	description:"Informacion sobre 15 tag de HTML y HTML5 importantes, tambien 20 atributos css3 para diseño."};
+let practice2 = {name:"Calendario Sem 5",
+	date:"25/08/2019.",
+	filename:"Lab03_Calendario/index.html",
+	description:"Calendario basado en web y javascript."};
+let practice3 = {name:"ConversorDivisas Sem 5",
+	date:"25/08/2019.",
+	filename:"Lab04_DenominationConverter/index.html",
+	description:"Convertidor monetario basado en web y javascript."};
