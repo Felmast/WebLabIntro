213281acce46c1ddb53ed51dd556cb25681cb2a1
Felmast <37111047+Felmast@users.noreply.github.com>
2019-08-26 12:02:52 -0600

    Add files via upload

---
+++ data/homeworks/fullHomeworkList.js
@@ -0,0 +1,2 @@
+let homework1 = {name:"A01-Full or Specialist WebDev?",date:"12/08/2019.",filename:"undefined",description:"Es posible que exista un Full stack developer?"};
+let homework2 = {name:"A02-Internet Evolution Timeline.",date:"12/08/2019.",filename:"TimelineWeb.jpg",description:"Linea de tiempo del desarrollo de la web."};
