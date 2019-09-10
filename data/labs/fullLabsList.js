c81ebd7f9f88a5eb9dad75296a5f321c66c1d2e0
Felmast <37111047+Felmast@users.noreply.github.com>
2019-09-09 18:58:08 -0600

    Add files via upload

---
+++ data/labs/fullLabsList.js
@@ -0,0 +1,4 @@
+﻿let lab1 = {name:"Eco-Bloques",
+	date:"09/09/2019.",
+	filename:"Eco-Bloques-PageDoneTabs/index.html",
+	description:"Pagina informativa sobre 'Eco-bloques basada en Bootstrap.'"};
