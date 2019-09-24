7c60dcf6f67628c87ce7f2dda6f3efeafc2d15b0
Felmast <37111047+Felmast@users.noreply.github.com>
2019-09-24 11:46:02 -0600

    Add files via upload

--- data/labs/fullLabsList.js
+++ data/labs/fullLabsList.js
@@ -2,3 +2,8 @@
 	date:"09/09/2019.",
 	filename:"Eco-Bloques-PageDoneTabs/index.html",
 	description:"Pagina informativa sobre 'Eco-bloques basada en Bootstrap.'"};
+	
+let lab2 = {name:"Creacion y utilizacion de mapas OSM",
+	date:"23/09/2019.",
+	filename:"Mapping",
+	description:"Mapa cargado de OSM con una ruta hecha con Leaflet.js"};
